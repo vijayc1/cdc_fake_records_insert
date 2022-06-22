2b46bcb4ae1e70a709dea3b481c7ede6e83e12d9
vijayc1 <42674883+vijayc1@users.noreply.github.com>
2022-06-22 19:51:53 +0530

    Update cdc_fake_records_insert.sql

--- cdc_fake_records_insert.sql
+++ cdc_fake_records_insert.sql
@@ -37,3 +37,36 @@ Begin
    Set @Id = @Id + 1
 End
 ;
+
+
+
+
+#Store Procedure
+
+
+CREATE PROCEDURE cdc_fake_records_sp
+AS
+Declare @Id int
+Set @Id = 1
+While @Id <= 1500
+Begin 
+   Insert Into cdc_fake_records values 
+   (
+   NEWID(),
+   'TeamId - ' + CAST(@Id as nvarchar(10)),
+   'TeamCode - ' + CAST(@Id as nvarchar(10)),
+   'FirstName - ' + CAST(@Id as nvarchar(10)),
+   'LastName  - ' + CAST(@Id as nvarchar(10)),
+   'Address1 - ' + CAST(@Id as nvarchar(10)),
+   'Address2 - ' + CAST(@Id as nvarchar(10)),
+   'City - ' + CAST(@Id as nvarchar(10)),
+   'State - ' + CAST(@Id as nvarchar(10)),
+   '60000' + CAST(@Id as nvarchar(10))
+   )
+   Set @Id = @Id + 1
+End
+GO
+;
+
+EXEC cdc_fake_records_sp;
+
